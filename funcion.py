7fe1ea4d517873d4d0b78596fd268b2878806a79
seanron123 <s.ron@alumno.etec.um.edu.ar>
2025-04-09 12:07:24 -0300

    aa

---
+++ funcion.py
@@ -0,0 +1,7 @@
+def resta():
+    numero1 = int(input("Ingrese el primer número: "))
+
+    numero2 = int(input("Ingrese el segundo número: "))
+    resultado = numero1 - numero2
+    print("El resultado de la resta es:", resultado)
+resta()
--- print("pepe")analizame el proyecto.py
+++ print("pepe")analizame el proyecto.py
@@ -1,1 +1,7 @@
 print("pepe")analizame el proyecto 
+def resta():
+    numero1 = 10
+    numero2 = 5
+    resultado = numero1 - numero2
+    print("El resultado de la resta es:", resultado)
+resta()
